c177afaf00906989646ee437aad0056cb62e6246
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-05-11 10:32:23 +0200

    these files shouldn't be maintained here

--- import_alto.pl
+++ import_alto.pl
@@ -5,10 +5,19 @@ use strict;
 my $source = "../trykkefrihedstekster/";
 my $destination = "./data/";
 
-if ( open(SRC,"find $source -name '*.xml' -typr f -print |") ) {
+if ( open(SRC,"find $source -name '*.xml' -type f -print |") ) {
 
     while(my $file = <SRC>) {
-	print $file;
+	chomp $file;
+	my $dest_file = $file;
+	my $directory = "";
+	$dest_file =~ s/$source//;
+	($directory,$dest_file) = split /\//, $dest_file;
+	$dest_file =~ m/^(\d+?)_/;
+	$directory =~ s/_$1//;
+	$dest_file = "$destination$directory/$dest_file";
+	print "mkdir -p $destination$directory; ";
+	print "cp $file $dest_file\n";
     }
 
 } else {

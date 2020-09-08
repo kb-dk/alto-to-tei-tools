f1844abc1f168ab547cf1b90a57bf34df4795072
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-09-08 18:03:16 +0200

    new improved data

--- import_alto.pl
+++ import_alto.pl
@@ -2,22 +2,28 @@
 
 use strict;
 
-my $source = "../trykkefrihedstekster/";
+my $source = "../trykkefrihedens-skrifter/";
 my $destination = "./data/";
 
 if ( open(SRC,"find $source -name '*.xml' -type f -print |") ) {
 
     while(my $file = <SRC>) {
 	chomp $file;
+	next unless $file =~ m/\d_\d\d\d/;
+	next unless $file;
 	my $dest_file = $file;
 	my $directory = "";
 	$dest_file =~ s/$source//;
 	($directory,$dest_file) = split /\//, $dest_file;
-	$dest_file =~ m/^(\d+?)_/;
-	$directory =~ s/_$1//;
-	$dest_file = "$destination$directory/$dest_file";
-	print "mkdir -p $destination$directory; ";
-	print "cp $file $dest_file\n";
+	if($dest_file =~ m/^(\d+?)_/) {
+	    $directory =~ s/_$1//;
+	    $dest_file = "$destination$directory/$dest_file";
+	    print "mkdir -p $destination$directory; ";
+	    print "cp $file $dest_file\n";
+	} else {
+	    print '# ' . $file . "\n";
+	    print "# $dest_file  does not parse\n";
+	}
     }
 
 } else {
